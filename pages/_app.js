451bd184e5d53bb651d844bc909b09daa80d53a0
aashirwad43 <aashirwad43@gmail.com>
2021-11-09 16:56:53 -0600

    Added manifest and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,38 @@
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
 import "mapbox-gl/dist/mapbox-gl.css";
+import React from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <React.Fragment>
+      <Head>
+        <link href="/manifest.webmanifest" rel="manifest" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 }
 
 export default MyApp;
